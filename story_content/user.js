e9c39fba68bbb4e1f02263d2de8d0a8a6a4eadf6
Milan Milasinovic <mmilasinovic@tpnyc.local>
2023-09-20 13:55:07 +0200

    v16

--- story_content/user.js
+++ story_content/user.js
@@ -2,76 +2,76 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6mW9TqKQTXe":
+      case "5fJ25ajlYNy":
         Script1();
         break;
-      case "6kEEnBdkgMV":
+      case "5fPVpt7IVCX":
         Script2();
         break;
-      case "6R8C7FJMWpu":
+      case "5spz109QWCz":
         Script3();
         break;
-      case "6gJDWMC2RiQ":
+      case "6PXeqeWMW3s":
         Script4();
         break;
-      case "5ghPUOHWcQ3":
+      case "5xQE0LO6oE7":
         Script5();
         break;
-      case "6pKWGuhOzWn":
+      case "6PC5WHtEwuo":
         Script6();
         break;
-      case "5qH9dzeg5hP":
+      case "5ubrxvEwjG1":
         Script7();
         break;
-      case "6eeUstFbf1r":
+      case "6MWQoeKo0QG":
         Script8();
         break;
-      case "5liNR8IrZTS":
+      case "5zsAJ3QbJ3o":
         Script9();
         break;
-      case "5lKZVSsfbTv":
+      case "5mZpHWAQ82t":
         Script10();
         break;
-      case "6jB5flYORxU":
+      case "6DQKs7DfUkG":
         Script11();
         break;
-      case "6LMGb1rHYsA":
+      case "5t3y4VFiZNc":
         Script12();
         break;
-      case "5cySHtVhnzc":
+      case "686ABEHoTex":
         Script13();
         break;
-      case "6aFOAUG5sgt":
+      case "5jqQ7suwaGa":
         Script14();
         break;
-      case "66190nlvJUB":
+      case "5qm2RCtOp9b":
         Script15();
         break;
-      case "5jW1BFafN1Q":
+      case "5ahjytMPCdY":
         Script16();
         break;
-      case "6CnGm4wSneg":
+      case "6UQvnRMg9LA":
         Script17();
         break;
-      case "6irsNrmK4k0":
+      case "6Ht4o52GoDz":
         Script18();
         break;
-      case "6qScxEEhTnJ":
+      case "6fmPxrV9Tnb":
         Script19();
         break;
-      case "6K2JxnwQ3YF":
+      case "6Ik1HHYGSPp":
         Script20();
         break;
-      case "60f4n5MJv59":
+      case "5vBRUyTjSTR":
         Script21();
         break;
-      case "5qMZ9t4EkpV":
+      case "6Ux5txTIzpQ":
         Script22();
         break;
-      case "6dysyFIWHVi":
+      case "6L1T156exdb":
         Script23();
         break;
-      case "6b0Gi7bZMwq":
+      case "64GVPrM2CB8":
         Script24();
         break;
   }
@@ -118,15 +118,15 @@ function Script5()
 {
   gtag('event', 'screen_view', {
   'app_name': 'myAppName',
-  'screen_name': 'Home'
+  'page_title': 'Slide1'
 });
 }
 
 function Script6()
 {
-  gtag('event', 'screen_view', {
+  gtag('event', 'slide_title', {
   'app_name': 'myAppName',
-  'screen_name': 'Home'
+  'page_title': 'Slide2'
 });
 }
 
@@ -159,12 +159,10 @@ function Script11()
 
 function Script12()
 {
-  //Get Var Pagetitle from Storyline
-var player = GetPlayer();
-var pagetitle = player.GetVar("PageTitle");
-
-//Send PageTitle to Analytics
-ga('send', 'screenview', {screenName: 'Slide4'});
+  gtag('event', 'slide_title', {
+  'app_name': 'myAppName',
+  'page_title': 'Slide1'
+});
 }
 
 function Script13()
