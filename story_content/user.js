bf16d0b020b7490cbbf5da128fcf937e41212c8a
Milan Milasinovic <mmilasinovic@tpnyc.local>
2023-09-21 15:02:58 +0200

    v24

--- story_content/user.js
+++ story_content/user.js
@@ -2,8 +2,11 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6YBjWFyTvSR":
+      case "6jHqwwU1NVP":
         Script1();
+        break;
+      case "6IIB3tZF4E7":
+        Script2();
         break;
   }
 }
@@ -12,6 +15,13 @@ function Script1()
 {
   const paths = [...document.getElementsByTagName('path')]
 paths[92].classList.add('Start')
-console.log(paths[92])
+console.log(paths)
+}
+
+function Script2()
+{
+  const paths = [...document.getElementsByTagName('path')]
+paths[94].classList.add('Next')
+console.log(paths)
 }
 
